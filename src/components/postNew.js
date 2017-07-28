fa997e75eaacd8e73db51d6d877e9892794ddd1a
Yago Pérez Vázquez <yagoperez@outlook.com>
2017-07-28 13:04:08 +0000

    Improving form

--- src/components/postNew.js
+++ src/components/postNew.js
@@ -6,23 +6,55 @@ class PostNew extends React.Component {
     super(props);
   }
 
-  renderTitleField(field) {
+  renderField(field) {
     return (
-      <div>
-        <input type="text" {...field.input} />
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        {field.meta.touched ? field.meta.error : ''}
       </div>
     );
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
-        <Field name="title" component={this.renderTitleField} />
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field label="Title" name="title" component={this.renderField} />
+        <Field label="Categories" name="categories" component={this.renderField} />
+        <Field label="Post Content" name="content" component={this.renderField} />
+        <button type="submit" className="btn btn-primary">Save</button>
       </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  //Validate values
+  if (!values.title || (values.title && values.title.length < 3)) {
+    errors.title = "Enter a title that is at least 3 characters";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter a title";
+  }
+  
+  if (!values.content) {
+    errors.content = "Enter a title";
+  }  
+  //If errors empty then the form is ok
+  //If errors contain propertys then Redux Form assumes the form is invalid
+  return errors;
+}
+
 export default reduxForm({
-  form: 'PostNewForm'
+  form: 'PostNewForm',
+  validate
 })(PostNew);
